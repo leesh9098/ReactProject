c2bd8574f979a55a56fbb4f16de07354fc0b310d
sung hoon lee <leesh9098@naver.com>
2021-08-27 17:37:54 +0900

    React Page Project

---
+++ src/loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Loader from "react-loader-spinner";
+
+function Loading() {
+    return (
+        <SpinContainer>
+            <Loader
+                type="Oval"
+                color="#3d66ba"
+                height={30}
+                width={30}
+                timeout={3000}
+            />
+        </SpinContainer>
+    );
+}
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import axios from "axios";
+// import Loading from "./loading";
 import "./Home.css";
 
 class Home extends React.Component {
     state = {
         isLoading: true,
     };
+
+    getPage = async () => {
+        const url = await axios.get()
+    }
+
+    render() {
+        return (
+            <div id="wrap">
+                
+            </div>
+        )
+    }
 }
 
 export default Home;
